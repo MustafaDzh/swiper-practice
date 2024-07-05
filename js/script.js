e8964f2ea1cc96657e8e671ca6354c6a709f5071
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-07-05 16:10:14 +0300

    swiper

--- js/script.js
+++ js/script.js
@@ -17,12 +17,35 @@ document.addEventListener('DOMContentLoaded', function () {
             },
         },
         on: {
-            slideChange: updateProgressBar,
+            slideChange: function () {
+                const progressWidth = `${(this.realIndex + 1) / this.slides.length * 100}%`;
+                document.querySelector('.progress-bar').style.width = progressWidth;
+            },
         },
     });
 
-    function updateProgressBar() {
-        const progressWidth = `${(swiper.realIndex + 1) / swiper.slides.length * 100}%`;
-        document.querySelector('.progress-bar').style.width = progressWidth;
-    }
+    const newSwiper = new Swiper('.swiper-container-1', {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.right-button-1',
+            prevEl: '.left-button-1'
+        },
+        pagination: {
+            el: ".swiper-pagination-1",
+            clickable: true,
+            dynamicBullets: true,
+            dynamicMainBullets: 6,
+            renderBullet: function (index, className) {
+                return `<span class="${className}">${index + 1}</span>`;
+            },
+        },
+        on: {
+            slideChange: function () {
+                const progressWidth = `${(this.realIndex + 1) / this.slides.length * 100}%`;
+                document.querySelector('.progress-bar-1').style.width = progressWidth;
+            },
+        },
+    });
 });
